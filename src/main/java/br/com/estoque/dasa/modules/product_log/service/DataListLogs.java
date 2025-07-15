886539e1e4d8c9a3b8c8129225e103f748e5a93f
Gabriel Gouvea <oliveirag396@gmail.com>
2025-07-15 00:46:26 -0300

    minor changes

--- src/main/java/br/com/estoque/dasa/modules/product_log/service/DataListLogs.java
+++ src/main/java/br/com/estoque/dasa/modules/product_log/service/DataListLogs.java
@@ -3,7 +3,7 @@ package br.com.estoque.dasa.modules.product_log.service;
 import br.com.estoque.dasa.modules.product.entity.Product;
 import br.com.estoque.dasa.modules.product_log.entity.ProductLog;
 
-public record DataListLogs(String id, String action, String cpf, Long quantity, Product product) {
+public record DataListLogs(String id, Action action, String cpf, Long quantity, Product product) {
 
     public DataListLogs(ProductLog productLog) {
         this(productLog.getId(), productLog.getAction(), productLog.getCpf(), productLog.getQuantity(), productLog.getProduct());
