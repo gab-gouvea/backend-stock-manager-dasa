58bcb909c9bc706cb6124d281095d15ca63dec58
Gabriel Gouvea <oliveirag396@gmail.com>
2025-08-21 21:15:53 -0300

    add: starting with rabbitmq consumer

--- src/main/java/br/com/estoque/dasa/modules/product/rabbitmq/ProductConsumer.java
+++ src/main/java/br/com/estoque/dasa/modules/product/rabbitmq/ProductConsumer.java
@@ -1,15 +1,13 @@
 package br.com.estoque.dasa.modules.product.rabbitmq;
 
 import br.com.estoque.dasa.modules.product_log.service.DataRemoval;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.core.util.Json;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import javax.sql.DataSource;
+
 
 @Component
 public class ProductConsumer {
@@ -19,8 +17,6 @@ public class ProductConsumer {
 
     @Autowired
     private ObjectMapper objectMapper;
-    @Autowired
-    private DataSource dataSource;
 
     @RabbitListener(queues = "fila.produto.atualizacao")
     public void receiveMessage(String message) {
@@ -30,11 +26,9 @@ public class ProductConsumer {
             restTemplate.postForObject("http://localhost:8080/products/removal", data, String.class);
 
             System.out.println("mensagem processada" + data);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            //da para tratar com logger aqui
+            e.printStackTrace();
         }
-
-
-
     }
 }
