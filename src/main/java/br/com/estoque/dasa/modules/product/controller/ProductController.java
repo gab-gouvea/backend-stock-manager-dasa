ea9cce062463eca3ef948dbb5946f1506daa58d3
Gabriel Gouvea <oliveirag396@gmail.com>
2025-08-22 22:11:31 -0300

    add: more endpoints for frontend

--- src/main/java/br/com/estoque/dasa/modules/product/controller/ProductController.java
+++ src/main/java/br/com/estoque/dasa/modules/product/controller/ProductController.java
@@ -10,6 +10,7 @@ import br.com.estoque.dasa.modules.product.service.DataAttProduct;
 import br.com.estoque.dasa.modules.product.service.DataCreateProduct;
 import br.com.estoque.dasa.modules.product.service.DataListProduct;
 import br.com.estoque.dasa.modules.product.entity.Product;
+import br.com.estoque.dasa.modules.product.service.DatalistProductMiguel;
 import br.com.estoque.dasa.modules.product_log.entity.ProductLog;
 import br.com.estoque.dasa.modules.product_log.repository.ProductLogRepository;
 import br.com.estoque.dasa.modules.product_log.service.DataJoin;
@@ -60,6 +61,11 @@ public class ProductController {
     @GetMapping
     public List<DataListProduct> list() {
         return repository.findAll().stream().map(DataListProduct::new).toList();
+    }
+
+    @GetMapping("/miguel")
+    public List<DatalistProductMiguel> listMiguel() {
+       return repository.findAll().stream().map(DatalistProductMiguel::new).toList();
     }
 
     @PutMapping
