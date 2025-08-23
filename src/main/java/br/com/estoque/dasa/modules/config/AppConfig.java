d207f7e1fde03bd40f10fdb0204e93e4adf4e552
Gabriel Gouvea <oliveirag396@gmail.com>
2025-08-22 22:21:29 -0300

    minor changes

--- src/main/java/br/com/estoque/dasa/modules/config/AppConfig.java
+++ src/main/java/br/com/estoque/dasa/modules/config/AppConfig.java
@@ -1,7 +1,5 @@
 package br.com.estoque.dasa.modules.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
