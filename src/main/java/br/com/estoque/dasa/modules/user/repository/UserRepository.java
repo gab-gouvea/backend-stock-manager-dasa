d6ab63bdda1b25312a1421f8676000e67a78bb39
Gabriel Gouvea <oliveirag396@gmail.com>
2025-08-28 22:55:51 -0300

    remove: security

--- src/main/java/br/com/estoque/dasa/modules/user/repository/UserRepository.java
+++ src/main/java/br/com/estoque/dasa/modules/user/repository/UserRepository.java
@@ -3,6 +3,9 @@ package br.com.estoque.dasa.modules.user.repository;
 import br.com.estoque.dasa.modules.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByEmail(String email);
+    Optional<User> findByName(String name);
 }
